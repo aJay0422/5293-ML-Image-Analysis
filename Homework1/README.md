8be812fe543b920d0079b7c74c7dfb88e5de29ff
shangjiedu <sd3497@columbia.edu>
2022-09-30 18:37:43 -0400

    Update single face solution

---
+++ Homework1/README.md
@@ -0,0 +1,58 @@
+# Code Explanation
+
+## Step 1
+Reand the image and convert it into LAB color space
+
+```python
+import cv2
+
+img = cv2.imread(imgPath + imgFilename)
+size = img.shape
+img_lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+```
+
+## Step 2
+Perform KMeans on AB dimension
+```python
+X = img_lab[:,:1:].reshape(size[0] * size[1], -1)
+kmeans = Kmeans(n_clusters=k, random_state=42)
+kmeans = kmeans.fit(X)
+cls = kmeans.predict(X)   # get cluster index for each pixel
+```
+
+## Step 3
+Calculate the cluster centers for each cluster in BGR space and find the cluster which is the closet
+to the target vector.
+```python
+masks = [(cls == i) for i in range(k)]
+cluster_means = []
+for i, mask in enumerate(masks):
+    cluster = img.reshape(size[0] * size[1], 3)[mask]
+    cluster_means.append(cluster.mean(axis=0))
+
+dits = []
+for cluster_mean in cluster_means:
+    dist = np.linalg.norm(cluster_mean - TARGET_COLOR, ord=2)
+    dists.append(dist)
+
+cluster_idx = np.argmin(dists)
+mask = masks[cluster_idx].reshape(size[0], size[1], 1)
+```
+Here is the cluster found after KMeans 
+![Mask](mask.jpg)
+
+## Step 4
+Remove noise in the cluster using DBSCAN. Here, the input of DBSCAN algorithm 
+is the coordinates of pixels, but not their color.  
+**Note**: We need to specify the argument `eps=2**0.5` for DBSCAN. It is the maximum distance
+between 2 samples for one to be considered as in the neighborhood of the other. Since the minimum 
+coordinates distance in an image is $\sqrt{2}$
+```python
+coords = []
+for i in range(size[0]):
+    for j in range(size[1]):
+        if mask[i, j]:
+            coords.append([i, j])
+coords = np.array(coords)
+db = DBSCAN(eps=2*0.5)
+```
--- Homework1/sd3497_Shangjie Du.py
+++ Homework1/sd3497_Shangjie Du.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
+import os
 from sklearn.cluster import KMeans, DBSCAN
 
 
 TARGET_COLOR = np.array([131, 152, 197])
 
 
-def UNI_Name_kmeans(imgPath,imgFilename,savedImgPath=None,savedImgFilename=None,k=3):
+def UNI_Name_kmeans(imgPath, imgFilename, savedImgPath=None, savedImgFilename=None, k=3):
     """
     K-means method for segmentation.
     :param imgPath: the path of the image folder. Please use relative path
@@ -40,16 +41,11 @@ def UNI_Name_kmeans(imgPath,imgFilename,savedImgPath=None,savedImgFilename=None,
         dist = np.linalg.norm(cluster_mean - TARGET_COLOR, ord=1)
         dists.append(dist)
 
-    # for i, mask in enumerate(masks):
-    #     mask = mask.reshape(size[0], size[1], 1)
-    #     cluster = img * mask
-    #     print(dists[i])
-    #     cv2.imshow(f"Cluster {i+1}", cluster)
-
-
     # Find the cluster which has minimum distance and plot that cluster
     cluster_idx = np.argmin(dists)
     mask = masks[cluster_idx].reshape(size[0], size[1], 1)
+    cv2.imshow("The Cluster", img * mask)
+    cv2.imwrite("mask.jpg", img * mask)
 
     # Find the bounding box
     coords = []
@@ -79,10 +75,46 @@ def UNI_Name_kmeans(imgPath,imgFilename,savedImgPath=None,savedImgFilename=None,
     mask_new = mask_new.astype("uint8")
     cv2.imshow("Segmentation", img * mask_new)
 
+    if not os.path.exists(savedImgPath):
+        os.mkdir(savedImgPath)
+    cv2.imwrite(savedImgPath + savedImgFilename, img_box)
+
+    cv2.waitKey()
+
+
+
+
+def multi_faces(imgPath, imgFileName):
+    import mediapipe as mp
+
+
+    mp_face_detection = mp.solutions.face_detection
+    mp_drawing = mp.solutions.drawing_utils
+    face_detection = mp_face_detection.FaceDetection(model_selection=1)
+
+    img = cv2.imread(imgPath + imgFilename)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = face_detection.process(imgRGB)
+
+    if result.detections:   # at least one face is detected
+        for id, detection in enumerate(result.detections):
+            ih, iw, ic = img.shape
+            bboxC = detection.location_data.relative_bounding_box
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                   int(bboxC.width * iw), int(bboxC.height * ih)
+            cv2.rectangle(img, bbox, color=(255, 0, 255), thickness=2)
+
+
+
+    cv2.imshow("faces", img)
     cv2.waitKey(0)
+
 
 
 if __name__ == "__main__":
     imgPath = "./"
-    imgFilename = "faces.jpg"
-    UNI_Name_kmeans(imgPath, imgFilename, k=5)
+    imgFilename = "face_d2.jpg"
+    savedImgPath = "./results/"
+    savedImgfilename = "face_d2_result.jpg"
+    k = 3
+    UNI_Name_kmeans(imgPath, imgFilename, savedImgPath, savedImgfilename, k=k)
