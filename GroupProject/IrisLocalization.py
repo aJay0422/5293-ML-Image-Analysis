200feb0720db7caf1fbad970012e8c00f0c5adb5
shangjiedu <sd3497@columbia.edu>
2022-10-18 16:00:43 -0400

    update group project

--- GroupProject/IrisLocalization.py
+++ GroupProject/IrisLocalization.py
@@ -32,28 +32,53 @@ def localization(img):
     :return:
     """
 
-    # Step 1: Estimate an approximate center
+    # Estimate an approximate center
     proj_h = np.mean(img, axis=0)   # horizontal projection
     proj_v = np.mean(img, axis=1)   # vertical projection
     X_p = np.argmin(proj_h)
     Y_p = np.argmin(proj_v)
 
-
-    # Step 2: Binarize
-
-    ## find the rectangle area
+    # Crop a 60x60 area around the center
     coord_upperleft = (X_p - 60, Y_p - 60)
-    coord_bottomright = (X_p + 60, Y_p + 60)   # the rectangle area has size (121, 121)
+    coord_botom_right = (X_p + 60, X_p + 60)
 
-    ## binarize with some threshold
+    # Binarize the cropped image with some threshold
     threshold = 50
-    pupil_coords = []   # record all coords with gray scale smaller than threshold
-    for x in range(coord_upperleft[0], coord_bottomright[0] + 1):
-        for y in range(coord_upperleft[1], coord_bottomright[1] + 1):
+    pupil_coords = []
+    for x in range(coord_upperleft[0], coord_botom_right[0] + 1)
+        for y in range(coord_upperleft[1], coord_botom_right[1] + 1):
             if img[y, x] <= threshold:
                 pupil_coords.append([x, y])
-    pupil_coords = np.array(pupil_coords)
-    centroid = np.mean(pupil_coords, axis=0)   # find the centroid
+
+    # Find the centroid of those pixels whose gray scale is below the threshold
+    centroid = np.mean(np.array(pupil_coords), axis=0, dtype=np.int64)
+    X_p, Y_p = centroid
+
+    # Repeat the crop and binarize process to get a better estimate of pupil center
+    coord_upperleft = (X_p - 60, Y_p - 60)
+    coord_botom_right = (X_p + 60, X_p + 60)
+    threshold = 50
+    pupil_coords = []
+    for x in range(coord_upperleft[0], coord_botom_right[0] + 1)
+        for y in range(coord_upperleft[1], coord_botom_right[1] + 1):
+            if img[y, x] <= threshold:
+                pupil_coords.append([x, y])
+
+    # Find the centroid of those pixels whose gray scale is below the threshold
+    centroid = np.mean(np.array(pupil_coords), axis=0, dtype=np.int64)
+    X_p, Y_p = centroid
+
+    # Find edge of the pupil and use hough circle to get the radius of pupil
+    img_crop = img[Y_p-60:Y_p+60, X_p-60:X_p+60].copy()
+    img_crop_bin = cv2.inRange(img_crop, 0, 50)
+    pupil_circles = cv2.HoughCircles(img_crop_bin, cv2.HOUGH_GRADIENT, 1, 300,
+                                     minRadius=20, maxRadius=60, param2=1)
+    pupil_circles = np,round(pupil_circles[0,:].astype("int"))
+    Rp = pupil_circles[0][2]
+
+
+
+
 
 
 if __name__ == "__main__":
--- GroupProject/scratch.py
+++ GroupProject/scratch.py
@@ -1,23 +1,20 @@
 import cv2
 import numpy as np
 
-
-file_path = "datasets/CASIA/001/1/001_1_1.bmp"
+# load image
+file_path = "datasets/CASIA/008/1/008_1_1.bmp"
 img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
 img_color = cv2.imread(file_path)
 
+# find an approximate center of pupil
 proj_h = np.mean(img, axis=0)
 proj_v = np.mean(img, axis=1)
-
 X_p = np.argmin(proj_h)
 Y_p = np.argmin(proj_v)
 
-
-coord_upperleft = (X_p - 120, Y_p - 120)
-coord_bottomright = (X_p + 120, Y_p + 120)
-
-img_color = cv2.rectangle(img_color, coord_upperleft, coord_bottomright, (255,0,0),
-              2)
+# crop a 60x60 area around the center
+coord_upperleft = (X_p - 60, Y_p - 60)
+coord_bottomright = (X_p + 60, Y_p + 60)
 
 ## binarize with some threshold
 threshold = 50
@@ -27,19 +24,75 @@ for x in range(coord_upperleft[0], coord_bottomright[0] + 1):
         if img[y, x] <= threshold:
             pupil_coords.append([x, y])
 
+# find the centroid of pixels whose gray scale is below the threshold
 centroid = np.mean(np.array(pupil_coords), axis=0, dtype=np.int64)
+X_p, Y_p = centroid
+
+# repeat binarize again and find a better estimate of pupil center
+coord_upperleft = (X_p - 60, Y_p - 60)
+coord_bottomright = (X_p + 60, Y_p + 60)
+threshold = 50
+pupil_coords = []
+for x in range(coord_upperleft[0], coord_bottomright[0] + 1):
+    for y in range(coord_upperleft[1], coord_bottomright[1] + 1):
+        if img[y, x] <= threshold:
+            pupil_coords.append([x, y])
+centroid = np.mean(np.array(pupil_coords), axis=0, dtype=np.int64)
+X_p, Y_p = centroid
+
+# crop a 120x120 area around the pupil center
+img_crop = img[Y_p-60:Y_p+60, X_p-60:X_p+60].copy()
+img_crop_color = img_color[Y_p-60:Y_p+60, X_p-60:X_p+60].copy()
+img_crop_bin = cv2.inRange(img_crop, 0, 50)
+cv2.imshow("Edge", img_crop_bin)
+cv2.waitKey(0)
+
+pupil_circles = cv2.HoughCircles(img_crop_bin, cv2.HOUGH_GRADIENT, 1, 300,
+                                 minRadius=20, maxRadius=60, param2=1)
+
+if pupil_circles is not None:
+    pupil_circles = np.round(pupil_circles[0,:]).astype("int")
+    for (x, y, r) in pupil_circles:
+        cv2.circle(img_crop_color, (x,y), r, color=(0,255,0), thickness=1)
+cv2.imshow("Cropped", img_crop_color)
+cv2.waitKey(0)
+
+Rp = pupil_circles[0][2]
+cv2.circle(img_color, (X_p, Y_p), Rp, color=(0,255,0), thickness=1)
+cv2.imshow("Pupil", img_color)
+cv2.waitKey(0)
+
+img_blurred = cv2.medianBlur(img, 7)
+img_edges = cv2.Canny(img_blurred, threshold1=15, threshold2=100)
+cv2.imshow("Edges", img_edges)
+cv2.waitKey(0)
+
+
+
+
+img_color = cv2.rectangle(img_color, coord_upperleft, coord_bottomright, (255,0,0),
+              2)
+img_color = cv2.circle(img_color, (X_p, Y_p), radius=2, color=[0,0,255])
+cv2.imshow("Image", img_color)
+cv2.waitKey(0)
+
+
+
 # img_color = cv2.circle(img_color, centroid, 2, (0,0,255), 3)
 
 img_cropped = img[centroid[1] - 120:centroid[1] + 120, centroid[0] - 120:centroid[0] + 120]
-cv2.imshow("Image", img_cropped)
+# cv2.imshow("Image", img_cropped)
+# cv2.waitKey(0)
 
 img_cropped = cv2.Canny(img_cropped, threshold1=75, threshold2=150)
 circles = cv2.HoughCircles(img_cropped, cv2.HOUGH_GRADIENT, 1, 1, param1=150, param2=1)
-circles = np.uint16(np.around(circles))
+circles = np.uint16(np.around(circles))[0]
 img_cropped_color = cv2.cvtColor(img_cropped, cv2.COLOR_GRAY2BGR)
-for i in circles[0,-1:]:
+for i in circles[:10]:
     img_cropped_color = cv2.circle(img_cropped_color, (i[0], i[1]), i[2],
                          (0,255,0), 2)
+    cv2.imshow("Image", img_cropped_color)
+    cv2.waitKey(0)
 
 
 
